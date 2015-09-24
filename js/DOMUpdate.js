db6c48a301c034c3ac6d073a278e20eee0751c50
Damien Calesse <damien.calesse@gmail.com>
2015-09-24 12:22:50 +0200

    Update DOMUpdate && Handler

--- js/DOMUpdate.js
+++ js/DOMUpdate.js
@@ -1,1 +1,14 @@
 /*Mise à jour de l'interface utilisateur*/
+
+var DOMUpdate = (function() {
+	function DOMUpdate() {
+		this.container = document.getElementById('container');
+	},
+
+	DOMUpdate.prototype.update = function(div, data) {
+		var element = this.find(div);
+		element.innerHTML(data);
+	}
+
+	return DOMUpdate;
+})();
